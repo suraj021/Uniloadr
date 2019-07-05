00c84bad842428ed6aaed447b8547fdf02e26589
srjbora <suraj.singh@elucidata.io>
2019-07-05 12:18:55 +0530

    [WIP]: instagram is working now

--- src/app/instagram/instagram.component.ts
+++ src/app/instagram/instagram.component.ts
@@ -13,55 +13,56 @@ export class InstagramComponent implements OnInit {
   loading: boolean;
   urlError: boolean;
   ready: boolean;
+  thumbnailUrl: string;
   constructor(private fetcher: FetcherService) { }
 
   ngOnInit() {
   }
 
-  download( input ) {
+  download(input) {
     let url = input.value;
-    this.loading= true;
-    this.urlError= false;
-    this.ready= false;
-    this.fetcher.get( 'api/video?url=' + url )
+    this.loading = true;
+    this.urlError = false;
+    this.ready = false;
+    this.fetcher.get('http://localhost:8080/api/video?url=' + url)
       .subscribe(
-        res=> {
-          if( res.killed=== false ){
-            this.urlError= true;
-            this.loading= false;
+        res => {
+          if (res.killed === false) {
+            this.urlError = true;
+            this.loading = false;
             return;
           }
+          this.thumbnailUrl = res['thumbnails'][0] ? res['thumbnails'][0]['url'] : '';
+          this.insta = new InstaVideo();
+          console.log(res);
+          for (let format of res['formats']) {
+            if (format.protocol === 'https' || format.protocol === 'http') {
+              let video = new Video();
+              video.height = format.height;
+              video.width = format.width;
+              video.ext = format.ext;
+              video.url = format.url;
 
-          this.insta= new InstaVideo();
-
-          for( let format of res ){
-            if( format.protocol=== 'https' || format.protocol=== 'http' ){
-              let video= new Video();
-              video.height= format.height;
-              video.width= format.width;
-              video.ext= format.ext;
-              video.url= format.url;
-
-              if( !this.insta.quality.has( video.width + ' x ' + video.height ) ){
-                this.insta.quality.set( video.width + ' x ' + video.height, new Array<Video>() );
-                this.insta.quality[video.width + ' x ' + video.height]= new Array<Video>();
+              if (!this.insta.quality.has(video.width + ' x ' + video.height)) {
+                this.insta.quality.set(video.width + ' x ' + video.height, new Array<Video>());
+                this.insta.quality[video.width + ' x ' + video.height] = new Array<Video>();
               }
 
-              this.insta.quality[video.width + ' x ' + video.height].push( video );
+              this.insta.quality[video.width + ' x ' + video.height].push(video);
 
             }
           }
-          this.loading= false;
-          this.ready= true;
-          console.log( this.insta );
+          this.loading = false;
+          this.ready = true;
+          console.log(this.insta);
 
         }
       );
 
   }
 
-  getAllKeys(){
-    return Array.from( this.insta.quality.keys() );
+  getAllKeys() {
+    return Array.from(this.insta.quality.keys());
   }
 
 }
